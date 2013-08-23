d0c83ba53648db41f4f85abdaad0344bd44ae41f
Darius I. Karel <me@dikarel.com>
2013-08-23 15:17:54 -0400

    Got roster context menu to selectively display options

--- TrickPlayBundle/Controller/TrickPlayController.php
+++ TrickPlayBundle/Controller/TrickPlayController.php
@@ -13,37 +13,52 @@ use Talis\SwiftForumBundle\Model\User;
  */
 class TrickPlayController extends Controller
 {
-
-
     /**
      * @Route("/roster", name="basic_roster")
      */
     public function rosterAction()
     {
+        $currentUserRole = $this->get('security.context')->getToken()->getUser()->getRole()->getRole();
         $role_map = $this->get('security.role_hierarchy')->getMap();
         $em = $this->getDoctrine()->getManager();
 
-        $users = $em->getRepository('TalisTrickPlayBundle:User')
-            ->findAll();
-
+        $users = $em->getRepository('TalisTrickPlayBundle:User')->findAll();
         $userMembers = array();
+
+        // Can only edit roster if higher than Member
+        $rosterEditable = in_array("ROLE_MEMBER", $role_map[$currentUserRole]);
+
+        // Can only select ranks that are below
+        $availableRanks = array(
+            "ROLE_ADMIN" => in_array("ROLE_ADMIN", $role_map[$currentUserRole]),
+            "ROLE_OFFICER" => in_array("ROLE_OFFICER", $role_map[$currentUserRole]),
+            "ROLE_MEMBER" => in_array("ROLE_MEMBER", $role_map[$currentUserRole]),
+            "ROLE_GUEST" => in_array("ROLE_GUEST", $role_map[$currentUserRole]),
+            "ROLE_BANNED" => in_array("ROLE_BANNED", $role_map[$currentUserRole])
+        );
 
         /** @var User $user */
         foreach($users as $user) {
+
+            // Only display member and higher
             if ( isset($role_map[$user->getRole()->getRole()]) &&
                 (   $user->getRole()->getRole() === 'ROLE_MEMBER' ||
                     in_array('ROLE_MEMBER', $role_map[$user->getRole()->getRole()])
                 ) ) {
 
-
+                $userRole = $user->getRole()->getRole();
                 $character = $em->getRepository('TalisTrickPlayBundle:Character')
                     ->findOneBy(array('user' => $user, 'isPrimary' => true));
 
+                // Can only edit users whose role is under the current user's
+                $editable = in_array($userRole, $role_map[$currentUserRole]);
+
                 if($character) {
-                    $userMembers[count($role_map[$user->getRole()->getRole()])][] = array('user' => $user, 'character' => $character);
+                    $userMembers[count($role_map[$userRole])][] = array('user' => $user, "editable" => $editable, 'character' => $character);
                 } else {
-                    $userMembers[count($role_map[$user->getRole()->getRole()])][] = array('user' => $user);
+                    $userMembers[count($role_map[$userRole])][] = array('user' => $user, "editable" => $editable);
                 }
+
                 unset($character);
             }
         }
@@ -51,6 +66,10 @@ class TrickPlayController extends Controller
         ksort($userMembers);
         $userMembers = array_reverse($userMembers);
 
-        return $this->render('TalisTrickPlayBundle:TrickPlay:roster.html.twig', array('users' => $userMembers, 'role_map' => $role_map ));
+        return $this->render('TalisTrickPlayBundle:TrickPlay:roster.html.twig', array(
+            'users' => $userMembers,
+            "rosterEditable" => $rosterEditable,
+            "availableRanks" => $availableRanks
+        ));
     }
 }
--- TrickPlayBundle/Resources/public/js/roster.js
+++ TrickPlayBundle/Resources/public/js/roster.js
@@ -1,10 +1,70 @@
-$(function() {
+var RosterEditor = (function() {
+  function init(allowEdit, availableRanks) {
+
+    // Hide rank changer if not allowed to edit roster
+    if (!allowEdit) {
+      $("#editor-rankchanger-divider").remove();
+      $("#editor-rankchanger").remove();
+    }
+
+    // Show dropdown on click
     $(".roster-user").click(function(e) {
-        $("#user-dropdown").css("top", e.pageY).css("left", e.pageX).show();
-        return false;
+      var username = $(this).data("username");
+      var editable = $(this).data("editable");
+      var role = $(this).data("role");
+      var id = $(this).data("id");
+
+      // If the same user is clicked twice, close dropdown
+      if (id == $("#editor").data("id") && $("#editor").is(":visible")) {
+        $("#editor").hide();
+        return;
+      }
+
+      $("#editor-username").text(username);
+
+      var editor = $("#editor");
+      editor.data("username", username).data("id", id);
+
+      if (editable) {
+        $("#editor .editor-sufficient").show();
+        $("#editor .editor-insufficient").hide();
+      } else {
+        $("#editor .editor-insufficient").show();
+        $("#editor .editor-sufficient").hide();
+      }
+
+      // Hide unavailable ranks
+      for (var field in availableRanks) {
+        var li = $("#editor li[data-role=" + field + "]");
+        if (!availableRanks[field]) li.hide();
+      }
+
+      // Highlight the appropriate list item
+      $("#editor .editor-sufficient").each(function() {
+        var listRole = $(this).data("role");
+        if (listRole == role) {
+          $(this).find("span").addClass("icon-circle").removeClass("icon-circle-blank");
+          $(this).addClass("roster-editor-active");
+        } else {
+          $(this).find("span").removeClass("icon-circle").addClass("icon-circle-blank");
+          $(this).removeClass("roster-editor-active");
+        }
+      })
+
+      editor.css("top", e.pageY).css("left", e.pageX).show();
+      return false;
     });
 
+    // Hide dropdown on outside click
     $(document).click(function() {
-        $("#user-dropdown").hide();
+      $("#editor").hide();
     })
-})
+
+    // Perform update on select
+    $(".editor-sufficient a").each(function() {
+      // TODO
+    })
+  }
+
+  return {init: init};
+})();
