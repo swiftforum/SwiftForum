8137ed8f1f9cdf87f391552254a9cf1d5f27cf6d
Darius I. Karel <me@dikarel.com>
2013-08-25 00:58:54 -0400

    Implemented frontpage persistence

--- TrickPlayBundle/Controller/HomeController.php
+++ TrickPlayBundle/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace Talis\TrickPlayBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Talis\SwiftForumBundle\Controller\HomeController as HomeControllerBase;
 use Talis\TrickPlayBundle\Entity\FrontPage;
 
@@ -19,8 +21,12 @@ class HomeController extends HomeControllerBase
      */
     public function indexAction()
     {
+        // Only officers or higher can edit frontpage
+        $currentUser = $this->get('security.context')->getToken()->getUser();
+        $editable = gettype($currentUser) == "object" && $currentUser->getRole();
+        $editable = $editable && ($currentUser->getRole()->getRole() == "ROLE_OFFICER" || in_array("ROLE_OFFICER", $this->get('security.role_hierarchy')->getPermittedMap($currentUser->getRole()->getRole())));
+
         $em = $this->getDoctrine()->getManager();
-        $editable = true;
         $frontPage = $em->getRepository("TalisTrickPlayBundle:FrontPage")->get();
         return $this->render('TalisTrickPlayBundle:Home:index.html.twig', array("frontPage" => $frontPage, "editable" => $editable));
     }
@@ -32,8 +38,20 @@ class HomeController extends HomeControllerBase
     public function editFrontpageAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $editable = true;
+        $markdown = Request::createFromGlobals()->request->get("markdown", "");
+        $currentUser = $this->get('security.context')->getToken()->getUser();
+
+        // Only officers or higher can edit frontpage
+        $authorized = gettype($currentUser) == "object" && $currentUser->getRole();
+        $authorized = $authorized && ($currentUser->getRole()->getRole() == "ROLE_OFFICER" || in_array("ROLE_OFFICER", $this->get('security.role_hierarchy')->getPermittedMap($currentUser->getRole()->getRole())));
+        if (!$authorized) return new JsonResponse(array("error" => "Unauthorized action"), 403);
+
+        // Update frontpage
         $frontPage = $em->getRepository("TalisTrickPlayBundle:FrontPage")->get();
-        return $this->render('TalisTrickPlayBundle:Home:index.html.twig', array("frontPage" => $frontPage, "editable" => $editable));
+        $frontPage->setMarkdown($markdown, $currentUser);
+        $em->persist($frontPage);
+        $em->flush();
+
+        return new JsonResponse(array("lastEdit" => $frontPage->getLastEdit(), "lastEditor" => array("username" => $frontPage->getLastEditor()->getUsername())));
     }
 }
