bd9953e2fabec7fc1fa028d2df3e4be0990476c4
Darius I. Karel <me@dikarel.com>
2013-09-02 23:03:03 -0400

    Fixed typo; implemented character update command

--- TrickPlayBundle/Command/LodestoneCharacterCommand.php
+++ TrickPlayBundle/Command/LodestoneCharacterCommand.php
@@ -2,11 +2,11 @@
 
 namespace Talis\TrickPlayBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class LodestoneCharacterCommand extends Command
+class LodestoneCharacterCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
@@ -15,7 +15,40 @@ class LodestoneCharacterCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // TODO
-        $output->writeln("HELLO WORLD");
+        $em = $this->getContainer()->get("Doctrine")->getManager();
+        $lodestone = $this->getContainer()->get("Lodestone");
+
+        $company = $em->getRepository('TalisTrickPlayBundle:LodestoneFreeCompany')->get();
+        if (!$company) {
+          $output->writeln("Error: Company data has not been loaded; please do that first");
+          return;
+        }
+
+        // Find stale character
+        $query = "SELECT c FROM TalisTrickPlayBundle:LodestoneCharacter c WHERE (c.freeCompany IS NOT NULL OR c.user IS NOT NULL) ORDER BY c.lastUpdated ASC";
+        $characters = $em->createQuery($query)->setMaxResults(1)->getResult();
+
+        if (empty($characters)) {
+          $output->writeln("No characters to update");
+          return;
+        }
+
+        // Get character data
+        $character = $characters[0];
+        $output->writeln("Getting character data for " . $character->getName() . "...");
+        $lodestoneData = $lodestone->getCharacter($character->getId());
+        if (!$lodestoneData) {
+            $output->writeln("Error: Cannot get character data.");
+            return;
+        }
+
+        $character->set($lodestoneData);
+
+        // Save character
+        $output->writeln("Saving character...");
+        $em->persist($character);
+        $em->flush();
+
+        $output->writeln("Success");
     }
 }
